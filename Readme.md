02bf6005e9bec12566300f5ab83badca5ec4c4b0
bumbu <bmbalex@gmail.com>
2014-04-18 02:26:11 +0300

    miner: add ability to specify date

--- Readme.md
+++ Readme.md
@@ -4,3 +4,4 @@
 * `cd` into project folder
 * run `npm install` to install all necessary node modules
 * run `node miner.js`
+* In order to specify a different from today day run `node miner.js -date="17 April, 2014"` where date should be a JS parsable string
--- miner.js
+++ miner.js
@@ -7,19 +7,46 @@ var request = require('request')
   , finishedSources = 0
 
   // Configuration data
-  sources = [
-    {
-      title: 'unimedia'
-    , url: 'http://unimedia.info/arhiva/stiri/17/04/2014'
-      //, url: 'http://unimedia.info/arhiva/stiri/{{dd}}/{{mm}}/{{YYYY}}'
-    , query: 'div > div > ul.no-list-type.fs14.bottom-separator > li > a.blue-orange'
+  , sources = [
+      {
+        title: 'unimedia'
+      , url: 'http://unimedia.info/arhiva/stiri/{{dd}}/{{mm}}/{{yyyy}}'
+      , query: 'div > div > ul.no-list-type.fs14.bottom-separator > li > a.blue-orange'
+      }
+    ]
+  , date = new Date()
+  , isValidDate = function(d) {
+      if ( Object.prototype.toString.call(d) !== "[object Date]" )
+        return false;
+      return !isNaN(d.getTime());
     }
-  ]
+  , pad = function(number, places) { // Works with max of 4 places
+      return ('0000' + number).slice(-places)
+    }
+
+// Look for parameters
+process.argv.forEach(function (val, index, array) {
+  // Look for date
+  if (val.indexOf('-date=') === 0) {
+    var _date = new Date(val.replace('-date=', ''))
+    if (isValidDate(_date)) {
+      date = _date
+    } else {
+      console.error("Your date is invalid, using default date");
+    }
+  }
+});
 
 // Process each source
 sources.forEach(function(source){
   (function(source){ // scope source variable
-    request(source.url, function(err, resp, body) {
+    // Parse url for date parameter
+    var _url = source.url
+    _url = _url.replace('{{dd}}', pad(date.getDate(), 2))
+    _url = _url.replace('{{mm}}', pad(date.getMonth() + 1, 2))
+    _url = _url.replace('{{yyyy}}', pad(date.getFullYear(), 4))
+
+    request(_url, function(err, resp, body) {
       if (err) throw err;
 
       $ = cheerio.load(body);
@@ -75,6 +102,5 @@ function countSentences(str) {
   return match ? match.length : 1;
 }
 
-// TODO limit by day (today or today an tomorrow)
 // TODO add more sources
 // TODO normalize diacritics
