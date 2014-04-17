201b187013bcf0c9b1b166fe6d5fd78a7c7958f8
bumbu <bmbalex@gmail.com>
2014-04-18 00:45:34 +0300

    update mining process

--- Readme.md
+++ Readme.md
@@ -1,10 +1,6 @@
 ## Data miner description
 
-1. Go to twitter developers https://dev.twitter.com/
-* Create an application
-* Generate access token
-* Set all necessary variables in `miner.js`
-* Install Node.js
+1. Install Node.js
 * `cd` into project folder
 * run `npm install` to install all necessary node modules
 * run `node miner.js`
--- miner.js
+++ miner.js
@@ -1,42 +1,48 @@
-var Config = {
-    api_key: 'StdgFCh9MruRVxIpsfFRY6ldR'
-  , api_secret: 'qrWKJTknqOuOtHbGreGHn6WXkPz316jidKsEZpoidl7AkOP9OH'
-  , access_token: '18399704-96fcLw8Rlb7azq3lIT1pfLyZuLVkdyYBdSEDBLvqU'
-  , access_token_secret: 'Tsdhhzh6gWaBReAA6Lr7YEncEv8Kr3cDY613W1cKZrEay'
-  }
-  , OAuth = require('OAuth')
-  , fs = require('fs');
+var request = require('request')
+  , cheerio = require('cheerio')
+  , fs = require('fs')
 
-var oauth = new OAuth.OAuth(
-  'https://api.twitter.com/oauth/request_token',
-  'https://api.twitter.com/oauth/access_token',
-  Config.api_key,
-  Config.api_secret,
-  '1.0A',
-  null,
-  'HMAC-SHA1'
-);
-oauth.get(
-  'https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=unimedia',
-  Config.access_token,
-  Config.access_token_secret,
-  function (e, data, res){
-    if (e) console.error(e);
+  // temporary variables
+  , minedData = []
+  , finishedSources = 0
 
-    var dataParsed = JSON.parse(data)
-    var usefulData = []
-
-    for (var d in dataParsed) {
-      usefulData.push({
-        phrase: dataParsed[d].text
-      , url: ''
-      , created_at: dataParsed[d].created_at
-      })
+  // Configuration data
+  sources = [
+    {
+      title: 'unimedia'
+    , url: 'http://unimedia.info/arhiva/stiri/17/04/2014'
+      //, url: 'http://unimedia.info/arhiva/stiri/{{dd}}/{{mm}}/{{YYYY}}'
+    , query: 'div > div > ul.no-list-type.fs14.bottom-separator > li > a.blue-orange'
     }
+  ]
 
-    // console.log(usefulData)
+// Process each source
+sources.forEach(function(source){
+  (function(source){ // scope source variable
+    request(source.url, function(err, resp, body) {
+      if (err) throw err;
 
-    fs.writeFile('./data_mined.json', JSON.stringify(usefulData), function(err) {
+      $ = cheerio.load(body);
+      $(source.query).each(function(index, element){
+        minedData.push({
+          phrase: $(this).text()
+        , url: $(this).attr('href')
+        })
+      })
+
+      finishSource()
+    });
+  })(source)
+})
+
+// Will increment finished sources flag
+// If all sources are done than save data to file
+function finishSource() {
+  finishedSources += 1
+
+  if (finishedSources >= sources.length) {
+    // Save JSON to file
+    fs.writeFile('./data_mined.json', JSON.stringify(minedData, null, 2), function(err) {
       if(err) {
         console.log(err);
       } else {
@@ -44,7 +50,15 @@ oauth.get(
       }
     });
   }
-);
+}
+
+var urlRegex = /\b((?:[a-z][\w-]+:(?:\/{1,3}|[a-z0-9%])|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}\/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'".,<>?«»“”‘’]))/ig;
+function extractLink(str) {
+  var match = str.match(urlRegex)
+
+  if (match && match.length > 0) return match[0];
+  return '';
+}
 
 // TODO limit by day (today or today an tomorrow)
 // TODO add more sources
@@ -54,3 +68,4 @@ oauth.get(
 // TODO normalize diacritics
 // TODO remove punctuation signs from the end of phrase
 // TODO mark prhases with multiple propositions
+*/
